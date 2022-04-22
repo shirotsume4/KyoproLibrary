a4f9fb96b9376f582d6bc44e0314106eda663973
Shirotsume <kassyan323@gmail.com>
2022-04-22 12:05:57 +0900

    first commit

---
+++ BinomalCoefficent.md
@@ -0,0 +1,39 @@
+# BinomalCoefficent
+二項係数のライブラリです。入力の最大値を $maxi$ とすると対し前計算が $ \Theta(maxi) $ かかります。デフォルトでは $maxi = 3 \times 10^6$ となっています。
+
+## コンストラクタ
+>hoge = BinomalCoefficent(MOD)\
+MOD はあらかじめ決められた <strong>素数</strong> です。\
+
+
+
+## choose
+>def choose(self, n, k) -> int:\
+>hoge.choose(n, k)\
+${}n C_k  \mod MOD$ を返します。
+
+### 制約
+$0 \le n < MOD$\
+$0 \le k < MOD$
+
+## perm
+>def perm(self, n, k) -> int:\
+>hoge.perm(n, k)\
+${}n P_k  \mod MOD$ を返します。
+
+### 制約
+$0 \le n < MOD$\
+$0 \le k < MOD$
+
+## homop
+>def homop(self, n, k) -> int:\
+>hoge.homop(n, k)\
+${}n H_k  \mod MOD$ を返します。
+
+### 制約
+$0 \le n < MOD$\
+$0 \le k < MOD$\
+$0 \le n + k - 1 < MOD$
+
+## 計算量
+計算量は、構築 $\Theta (maxi)$ 、 クエリ $\Theta (1) $ です。
---
+++ BinomalCoefficent.py
@@ -0,0 +1,25 @@
+class BinomialCoefficient():
+    def __init__(self, mod, maxi = 3 * 10 ** 6):
+        self.mod = mod
+        self.maxi = maxi
+        self.facs = [1, 1]
+        self.factinvs = [1, 1]
+        self.invs = [0, 1]
+        for i in range(2, self.maxi + 1):
+            self.facs.append((self.facs[-1] * i) % self.mod)
+            self.invs.append((-self.invs[self.mod % i] * (self.mod // i)) % self.mod)
+            self.factinvs.append((self.factinvs[-1] * self.invs[-1]) % self.mod)
+    def choose(self, n, k) -> int:
+        if k < 0 or k > n: return 0
+        if k == 0 or k == n: return 1
+        k = min(k, n - k)
+        return (((self.facs[n] * self.factinvs[k]) % self.mod) * self.factinvs[n-k]) % self.mod
+    
+    def perm(self, n, k) -> int:
+        return (self.choose(n, k) * self.facs[k]) % self.mod
+
+    def homop(self, n, k) -> int:
+        if n == k == 0:
+            return 1
+        return self.choose(n + k - 1, k)
+
