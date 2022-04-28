7b7f1bd8a4a3924e4f9d6e76956c31980c152809
Shirotsume <kassyan323@gmail.com>
2022-04-28 12:07:44 +0900

    master

--- matrix.py
+++ matrix.py
@@ -3,7 +3,7 @@ class matrix():
     c = 1
     A = None
     mod = None
-    def __init__(self, r, c, mod = None, one = False, A = None):
+    def __init__(self, r, c, mod = 998244353, one = False, A = None):
         self.r = r
         self.c = c
         self.A = [[0] * self.c for _ in range(self.r)]
@@ -31,8 +31,7 @@ class matrix():
         for i in range(self.r):
             for j in range(self.c):
                 ret[i, j] = self[i, j] + other[i, j]
-                if self.mod is not None:
-                    ret[i, j] %= self.mod
+                ret[i, j] %= self.mod
         return ret
 
     def __sub__(self, other):
@@ -42,8 +41,7 @@ class matrix():
         for i in range(self.r):
             for j in range(self.c):
                 ret[i, j] = self[i, j] - other[i, j]
-                if self.mod is not None:
-                    ret[i, j] %= self.mod
+                ret[i, j] %= self.mod
         return ret
 
     def __mul__(self, other):
@@ -54,8 +52,7 @@ class matrix():
             for j in range(self.c):
                 for k in range(other.c):
                     ret[i, k] += self[i, j] * other[j, k]
-                    if self.mod is not None:
-                        ret[i, k] %= self.mod
+                    ret[i, k] %= self.mod
         return ret
 
     def augment(self, other):
@@ -67,7 +64,81 @@ class matrix():
         for i in range(self.r):
             X.append((self.A[i] + other.A[i]))
         
-        return matrix(self.r, 2 * self.c, mod = self.mod, A = X)
+        return matrix(self.r, self.c + other.c, mod = self.mod, A = X)
+    
+    def diminish(self, c):
+
+        X = []
+
+        for i in range(self.r):
+            X.append((self.A[i][:c]))
+        
+        return matrix(self.r, c, mod = self.mod, A = X)
+        
+    def hakidashi(self):
+        for i in range(self.c):
+            for j in range(i, self.r):
+                if self.A[j][i] != 0:
+                    for k in range(self.c):
+                        self.A[j][k], self.A[i][k] = self.A[i][k], self.A[j][k]
+                    break
+            if i >= self.r or self.A[i][i] == 0:
+                continue
+            K = pow(self.A[i][i], self.mod - 2, self.mod)
+            for k in range(self.c):
+                self.A[i][k] *= K
+                self.A[i][k] %= self.mod
+            assert self.A[i][i] == 1
+            for j in range(self.r):
+                if j == i: continue
+                K = self.A[j][i]
+                for k in range(self.c):
+                    self.A[j][k] -= self.A[i][k] * K
+                    self.A[j][k] %= self.mod
+        return self
+        
+
+        
+
+
+
+    def lineareq(self, b):
+        assert self.r == b.r
+        assert b.c == 1
+        M = self.augment(b)
+        M.hakidashi()
+        ans = [0] * self.c
+        ker = []
+
+        for i in range(self.r):
+            cnt = 0
+            for j in range(self.c):
+                cnt = max(cnt, M.A[i][j])
+            if cnt == 0 and M.A[i][-1] > 0:
+                return -1, -1
+
+        for i in range(self.c):
+            if i >= self.r:
+                break
+            if M.A[i][i] == 1:
+                ans[i] = M.A[i][-1] % self.mod
+            else:
+                ans[i] = 0
+
+        for i in range(self.r):
+            if self.A[i][i] != 1:
+                ret = []
+                for x in range(self.c):
+                    if x == i:
+                        ret.append((-1) % self.mod)
+                    else:
+                        ret.append((M.A[i][x]) % self.mod)
+                ker.append(ret)
+        return ans, ker
+                    
+
+
+        
 
 
 
@@ -76,3 +147,35 @@ class matrix():
         for v in self.A:
             print(*v)
 
+
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+ii = lambda: int(input())
+mi = lambda: map(int, input().split())
+li = lambda: list(mi())
+INF = 2 ** 63 - 1
+mod = 998244353
+n, m = mi()
+a = matrix(n, m)
+
+for i in range(n):
+    al = li()
+    for j in range(m):
+        a[i, j] = al[j]
+
+b = matrix(n, 1)
+bl = li()
+
+for i in range(n):
+    b[i, 0] = bl[i]
+
+ans, ker = a.lineareq(b)
+if ans == -1:
+    print(-1)
+    exit()
+print(len(ker))
+
+print(*ans)
+
+for v in ker:
+    print(*v)
---
+++ xorconvolution.py
@@ -0,0 +1,48 @@
+class xorconvolution():
+    def __init__(self, mod = 998244353):
+        self.mod = mod
+
+    def FHT(self, a):
+        n = len(a)
+        i = 1
+        while i < n:
+            for j in range(n):
+                if (j & i) == 0:
+                    x = a[j]
+                    y = a[j | i]
+                    a[j] = x + y
+                    a[j | i] = x - y
+            i *= 2
+
+    def IFHT(self, a):
+        inv2 = pow(2, self.mod - 2, self.mod)
+        n = len(a)
+        i = 1
+        while i < n:
+            for j in range(n):
+                if (j & i) == 0:
+                    x = a[j]
+                    y = a[j | i]
+                    a[j] = x + y
+                    a[j | i] = x - y
+                    a[j] *= inv2; a[j | i] *= inv2
+                    a[j] %= self.mod
+                    a[j | i] %= self.mod
+            i *= 2
+        
+    def XORconv(self,a, b):
+        n = len(a)
+        self.FHT(a)
+        self.FHT(b)
+        c = [0] * n
+
+        for i in range(n):
+            c[i] = (a[i] * b[i]) % self.mod
+        
+        self.IFHT(c)
+
+        for i in range(n):
+            c[i] %= self.mod
+        return c
+
+
