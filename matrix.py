cb4582d4dbf79f8ffbb494404c96183c9d36ceb3
Shirotsume <kassyan323@gmail.com>
2022-04-29 16:51:25 +0900

    master

--- matrix.py
+++ matrix.py
@@ -1,22 +1,15 @@
 class matrix():
-    r = 1
-    c = 1
-    A = None
-    mod = None
-    def __init__(self, r, c, mod = 998244353, one = False, A = None):
+    def __init__(self, r, c, mod = 998244353):
         self.r = r
         self.c = c
         self.A = [[0] * self.c for _ in range(self.r)]
-        if A is not None:
-            for i in range(self.r):
-                for j in range(self.c):
-                    self.A[i][j] = A[i][j]
-        if one:
-            for i in range(min(self.r, self.c)):
-                self.A[i][i] = 1
-        if mod is not None:
-            self.mod = mod
+        self.mod = mod
     
+    def makeone(r = 1):
+        A = matrix(r, r, mod)
+        for i in range(r):
+            A[i, i] = 1
+        return A
     def __getitem__(self, key):
         rnow, cnow = key
         return self.A[rnow][cnow]
@@ -59,12 +52,15 @@ class matrix():
 
         assert self.r == other.r
 
-        X = []
+        X = matrix(self.r, self.c + other.c, mod = self.mod)
 
         for i in range(self.r):
-            X.append((self.A[i] + other.A[i]))
+            for j in range(self.c):
+                X[i, j] = self[i, j]
+            for j in range(other.c):
+                X[i, j + self.c] = other[i, j]
         
-        return matrix(self.r, self.c + other.c, mod = self.mod, A = X)
+        return X
     
     def diminish(self, c):
 
@@ -77,105 +73,126 @@ class matrix():
         
     def hakidashi(self):
         for i in range(self.c):
-            for j in range(i, self.r):
-                if self.A[j][i] != 0:
+            for j in range(i + 1, self.r):
+                if self[j, i] != 0:
                     for k in range(self.c):
-                        self.A[j][k], self.A[i][k] = self.A[i][k], self.A[j][k]
+                        self[j, k], self[i, k] = self[i, k], self[j, k]
                     break
-            if i >= self.r or self.A[i][i] == 0:
+
+        for i in range(self.r):
+            for j in range(self.c):
+                if self[i, j] != 0:
+                    break
+            else:
                 continue
-            K = pow(self.A[i][i], self.mod - 2, self.mod)
-            for k in range(self.c):
-                self.A[i][k] *= K
-                self.A[i][k] %= self.mod
-            assert self.A[i][i] == 1
-            for j in range(self.r):
-                if j == i: continue
-                K = self.A[j][i]
-                for k in range(self.c):
-                    self.A[j][k] -= self.A[i][k] * K
-                    self.A[j][k] %= self.mod
+            K = pow(self[i, j], self.mod - 2, self.mod)
+
+            for to in range(self.c):
+                self[i, to] *= K
+                self[i, to] %= self.mod
+
+            for i2 in range(self.r):
+                if i == i2:
+                    continue
+                time = self[i2, j]
+                for j2 in range(self.c):
+                    self[i2, j2] -= time * self[i, j2]
+                    self[i2, j2] %= self.mod
+
         return self
+
+    def inv(self):
+        assert self.c == self.r
+
+        one = matrix.makeone(r = self.r)
+        new = self.augment(one)
+        new.hakidashi()
+        for i in range(self.r):
+            for j in range(self.c):
+                if i == j:
+                    if new[i, j] != 1:
+                        return 0, new
+                else:
+                    if new[i, j] != 0:
+                        return 0, new
         
+        X = matrix(self.r, self.c)
 
-        
+        for i in range(self.r):
+            for j in range(self.c):
+                X[i, j] = new[i, j + self.c]
 
-
+        return 1, X
 
     def lineareq(self, b):
         assert self.r == b.r
         assert b.c == 1
-        M = self.augment(b)
-        M.hakidashi()
+        Y = self.augment(b)
+        Y = Y.hakidashi()
+        B = [[0] * self.c for _ in range(self.c)]
         ans = [0] * self.c
-        ker = []
 
+        flag = [0] * self.c
         for i in range(self.r):
-            cnt = 0
-            for j in range(self.c):
-                cnt = max(cnt, M.A[i][j])
-            if cnt == 0 and M.A[i][-1] > 0:
-                return -1, -1
-
+            j = 0
+            while j < self.c and Y[i, j] == 0:
+                j += 1
+            if j == self.c:
+                if Y[i, -1] != 0:
+                    return None, None
+                continue
+            flag[j] = 1
+            ans[j] = Y[i, -1]
+            for k in range(j + 1, self.c):
+                if Y[i, k] % self.mod != 0:
+                    B[k][j] = (-Y[i, k])% self.mod
+                    flag[k] = -1
         for i in range(self.c):
-            if i >= self.r:
-                break
-            if M.A[i][i] == 1:
-                ans[i] = M.A[i][-1] % self.mod
-            else:
-                ans[i] = 0
+            if  flag[i] != 1:
+                B[i][i] = 1
+        B=[B[i] for i in range(self.c) if flag[i] != 1]
+        return ans,B
 
+    def rank(self):
+        new = self.hakidashi()
+        ret = 0
         for i in range(self.r):
-            if self.A[i][i] != 1:
-                ret = []
-                for x in range(self.c):
-                    if x == i:
-                        ret.append((-1) % self.mod)
-                    else:
-                        ret.append((M.A[i][x]) % self.mod)
-                ker.append(ret)
-        return ans, ker
-                    
+            for j in range(self.c):
+                if new[i, j] != 0:
+                    ret += 1
+                    break
+        return ret
 
+    def det(self):
+        ret = 1
+        a = self
+        for i in range(self.r):
+            if a[i, i] == 0:
+                for j in range(i + 1, self.r):
+                    if a[j, i]:
+                        break
+                else:
+                    return 0
+                for k in range(self.r):
+                    a[j, k], a[i, k] = a[i, k], a[j, k]
+                ret *= -1
+                ret %= self.mod
 
-        
+            for j in range(self.r):
+                if i < j:
+                    buf = a[j, i] * (pow(a[i, i], self.mod - 2, self.mod))
+                    buf %= self.mod
+                    for k in range(self.r):
+                        a[j, k] -= a[i, k] * buf
 
-
-
+                        a[j, k] %= self.mod
+        for i in range(self.r):
+            ret *= a[i, i]
+            ret %= self.mod
+        return ret
 
     def print(self):
         for v in self.A:
             print(*v)
 
-
-import sys
-input = lambda: sys.stdin.readline().rstrip()
-ii = lambda: int(input())
-mi = lambda: map(int, input().split())
-li = lambda: list(mi())
-INF = 2 ** 63 - 1
-mod = 998244353
-n, m = mi()
-a = matrix(n, m)
-
-for i in range(n):
-    al = li()
-    for j in range(m):
-        a[i, j] = al[j]
-
-b = matrix(n, 1)
-bl = li()
-
-for i in range(n):
-    b[i, 0] = bl[i]
-
-ans, ker = a.lineareq(b)
-if ans == -1:
-    print(-1)
-    exit()
-print(len(ker))
-
-print(*ans)
-
-for v in ker:
-    print(*v)
+        
