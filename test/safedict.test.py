7f77ef626daf8244f82c83bb7f307b17a6c4ad49
Shirotsume <kassyan323@gmail.com>
2022-11-02 13:05:13 +0900

    master

--- test/safedict.test.py
+++ test/safedict.test.py
@@ -6,17 +6,7 @@ mi = lambda: map(int, input().split())
 li = lambda: list(mi())
 inf = 2 ** 63 - 1
 mod = 998244353
-class sdict(dict):
-    def __init__(self):
-        import random
-        self.r = random.randint(1, 2 ** 63 - 1)
-    def __contains__(self, __o: object) -> bool:
-        return super().__contains__(__o^self.r)
-    def __getitem__(self, __key):
-        return super().__getitem__(__key^self.r)
-    def __setitem__(self, __key, __value):
-        return super().__setitem__(__key^self.r, __value)
-
+from .. import safedict
 q = ii()
 d = sdict()
 for _ in range(q):
